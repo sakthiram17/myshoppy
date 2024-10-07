8497f43648f93dfe80e280e07cfee341eb01625b
Ram <sakthi.ram@accenture.com>
2024-10-07 10:46:52 +0530

    initial build

---
+++ App.tsx
@@ -0,0 +1,117 @@
+import { StatusBar } from 'expo-status-bar';
+import { Alert, Button, FlatList, StyleSheet, Text, TextInput, View } from 'react-native';
+import { ActivityIndicator } from 'react-native';
+import BookCard from './Components/BookCard';
+import React,{ useState,useEffect } from 'react';
+import axios from 'axios';
+
+interface Books{
+  title:string,
+  price:string,
+  image:string,
+  isbn13:string
+}
+export default function App() {
+  const [searchTerm,setSearchTerm] = useState<string>('')
+  const [books,setBooks] = useState<Books []>([])
+  const [spinner,setSpinner] = useState(false)
+
+  const fetchResultsHandler = async ()=>{
+    if(searchTerm==='')
+    {
+      Alert.alert('Please enter a Search Term')
+      return
+    }
+    let response;
+    setSpinner(true)
+    try{
+      response = await axios.get('https://api.itbook.store/1.0/search/'+searchTerm)
+      setBooks(response.data["books"])
+      setSpinner(false)
+    
+    }
+    catch(err:any)
+    {
+      setSpinner(false)
+      alert('Cannot Fetch data from server, please try again later')
+    }
+  }
+
+  const removeBookHandler = (isbn13:string)=>{
+    let prev =[...books]
+    prev = (prev.filter((book)=>{
+      return book.isbn13 !== isbn13
+    }))
+    setBooks(prev)
+    
+
+  }
+
+
+
+
+  return (
+    <View style={styles.container}>
+      <View style={styles.inputContainer}>
+        <TextInput placeholder='Enter a word to search'
+         style = {styles.textInput}
+         value={searchTerm}
+         onChangeText={(enteredText)=>{
+          setSearchTerm(enteredText)
+         }}
+        ></TextInput>
+        <View style={styles.buttonContainer}>
+        <Button title="Search" onPress={fetchResultsHandler}></Button>
+        </View>
+      </View>
+      {spinner?<ActivityIndicator size='large'></ActivityIndicator>:null}
+      <View style={styles.cardsContainer}>
+        {books.length>0?  <FlatList
+        data={books}
+        renderItem={({item})=>{
+          return <BookCard name={item.title} image={item.image} price={item.price} isbn13={item.isbn13} onRemove={removeBookHandler}></BookCard>
+        }}
+        keyExtractor={(books)=>books.isbn13} 
+        ></FlatList> : <Text>No Matching books found, try again with simpler search</Text>}
+      
+      </View>
+
+
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  inputContainer:{
+    width:"100%",
+    flex:3,
+    marginTop:48,
+    justifyContent:'center',
+    alignItems:'center'
+  },
+  cardsContainer:{
+    flex:14,
+    justifyContent:'center',
+    alignItems:'center'
+  },
+  textInput:{
+    padding:4,
+    paddingHorizontal:12,
+    borderColor:'grey',
+    borderWidth:1,
+    margin:12,
+    width:"90%",
+    marginVertical:24
+  },
+  buttonContainer:{
+    width:'90%',
+  
+  }
+});
---
+++ Components/BookCard.tsx
@@ -0,0 +1,82 @@
+import { StyleSheet, Text, View ,Image, Button} from 'react-native'
+import React from 'react'
+interface BookCardProps{
+    name:string,
+    price:string,
+    image:string,
+    isbn13:string,
+    onRemove:(isbn13:string)=>void
+  }
+const BookCard = ({image,name,price,isbn13,onRemove}:BookCardProps) => {
+  return (
+    <View style={styles.bookCard}>
+        <View style={styles.imageContainer}>
+        <Image source={{uri:image}} style={styles.image}>
+        </Image>
+        </View>
+      <View style={styles.textContainer}>
+        <View style={styles.bookNameContainer}>
+        <Text style={styles.bookName}>{name}</Text>
+        </View>
+        <Text style={styles.Price}>{price}</Text>
+        <View style={{margin:8}}>
+        <Button title='Remove' color="orangered" onPress={()=>{
+            onRemove(isbn13)
+        }}></Button>
+        </View>
+        </View>
+    
+    </View>
+  )
+}
+
+export default BookCard
+
+const styles = StyleSheet.create({
+    imageContainer:{
+        flex:1
+    },
+    image:{
+        height:140,
+        width:'auto'
+    },
+    bookCard:{
+        backgroundColor:"white",
+        borderWidth:1,
+        borderColor:"#ccc",
+        flexDirection:'row',
+        alignItems:'center',
+        justifyContent:'space-evenly',
+        marginVertical:24,
+        marginHorizontal:1,
+        borderRadius:12,
+        
+        width:'98%',
+        height:'auto',
+       
+        
+    },
+    bookName:{
+        fontWeight:'bold',
+        fontSize:24,
+        flexWrap:'wrap',
+        flexDirection:'row',
+        margin:4
+    },
+    Price:{
+        color:'grey',
+        fontSize:16,
+        margin:4
+    },
+    textContainer:{
+       justifyContent:'space-between',
+       flexWrap:'wrap',
+       maxWidth:'60%'
+        
+    },
+    bookNameContainer:{
+        flexDirection:'row',
+        flexWrap:'wrap',
+
+    }
+})
